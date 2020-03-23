5a35ded0a9ebce47f0a29447085d5dd62a6a1992
Tristen Everett <spock214@gmail.com>
2020-03-23 09:30:58 -0700

    change from seperate inputs for each function there is now one that can do them all

--- js/scripts.js
+++ js/scripts.js
@@ -15,35 +15,21 @@ var divide = function (number1, number2) {
 };
 
 jQuery(document).ready(function(){
-  jQuery("form#add").submit(function(event){
+  jQuery("form#calculator").submit(function(event){
     event.preventDefault();
-    var number1 = parseInt(jQuery("#add1").val());
-    var number2 = parseInt(jQuery("#add2").val());
-    var result = add(number1, number2);
-    jQuery("#output").text(number1 + " + " + number2 + " = " + result);
-  });
-
-  jQuery("form#subtract").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt(jQuery("#subtract1").val());
-    var number2 = parseInt(jQuery("#subtract2").val());
-    var result = subtract(number1, number2);
-    jQuery("#output").text(number1 + " - " + number2 + " = " + result);
-  });
-
-  jQuery("form#multiply").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt(jQuery("#multiply1").val());
-    var number2 = parseInt(jQuery("#multiply2").val());
-    var result = multiply(number1, number2);
-    jQuery("#output").text(number1 + " * " + number2 + " = " + result);
-  });
-
-  jQuery("form#divide").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt(jQuery("#divide1").val());
-    var number2 = parseInt(jQuery("#divide2").val());
-    var result = divide(number1, number2);
-    jQuery("#output").text(number1 + " / " + number2 + " = " + result);
+    var number1 = parseInt(jQuery("#input1").val());
+    var number2 = parseInt(jQuery("#input2").val());
+    var operator = jQuery("input:radio[name=operator]:checked").val();
+    var result;
+    if(operator === "add"){
+      result = add(number1, number2);
+    } else if(operator ==="subtract"){
+      result = subtract(number1, number2);
+    } else if(operator === "multiply"){
+      result = multiply(number1, number2);
+    } else if(operator === "divide"){
+      result = divide(number1, number2);
+    };
+    jQuery("#output").text(result);
   });
 });
